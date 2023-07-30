5e114a4b1bbf294284333de09fd7a0a76325c3f4
dariomiguel <101842755+dariomiguel@users.noreply.github.com>
2023-07-30 14:50:06 -0300

    Agregar component ItemCount

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import NavBar from "./components/NavBar/NavBar";
+import ItemCount from "./components/ItemCount/itemCount";
 import "../node_modules/bulma/css/bulma.css"
 
 function App(){
@@ -7,6 +8,7 @@ function App(){
         <div className="App">
             <NavBar/>
             <ItemListContainer greeting={"Bienvenido a Capyart!!!"}/>
+            <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log("Cantidad agregada ", quantity)}/>
         </div>
     )
 }
---
+++ src/components/ItemCount/itemCount.js
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+const ItemCount = ({stock, initial, onAdd})=>{
+    const [cantidad, setCantidad] = useState(initial)
+
+    const incrementar = () => cantidad < stock ? setCantidad(cantidad + 1):null;
+    const disminuir = () => cantidad > 1 ? setCantidad(cantidad - 1):null;
+
+    return(
+        <div className="columns is-mobile">
+            <div class="column is-half is-offset-one-quarter">
+                <div className="column">
+                    <div className="buttons has-addons is-centered">
+                        <button className="button" onClick={disminuir}>-</button>
+                        <h4 className="button">{cantidad}</h4>
+                        <button className="button" onClick={incrementar}>+</button>
+                        <button className="button" onClick={() => onAdd(cantidad)} disabled={!stock}>
+                            Agregar al Carrito
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ItemCount;
